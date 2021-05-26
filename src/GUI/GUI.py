15ee99cd66dae844ffb9c757dc9afaf9b75f4535
robin <robin.enjalbrt@gmail.com>
2021-05-26 21:32:59 +0100

    Choose trump values in GUI.

--- src/GUI/GUI.py
+++ src/GUI/GUI.py
@@ -16,6 +16,7 @@ class GUI:
         self.font = pygame.font.SysFont("Arial", 30)
         self.show_background()
         self.contract_clicked = False
+        self.trump_clicked = False
 
     def show_background(self):
         self.display.blit(self.images.background, (0, 0))
@@ -40,8 +41,9 @@ class GUI:
         prev_contract, trump = bidding[0], bidding[1]
         contract = prev_contract
         # 1) Display the values and trumps
-        contract_buttons = self.show_bidding_window(prev_contract)
+        contract_buttons, trump_buttons = self.show_bidding_window(prev_contract)
         contract_box = pygame.Rect(CONTRACT_BOX)
+        trump_box = pygame.Rect(TRUMP_BOX)
         # 2) While no choice, wait for player validation
         validation = False
         while not validation:
@@ -54,14 +56,17 @@ class GUI:
                         if contract is not None:
                             print(CONTRACTS[contract + 1])
                     # Click on trump
-                    # Todo
+                    elif trump_box.collidepoint(pointer):
+                        trump = self.click_trump(pointer, trump_buttons, trump)
+                        if trump is not None:
+                            print(TRUMPS[trump])
                     # Click on validation button
                     # Todo
         return contract, trump
 
     def show_bidding_window(self, contract):
         # Draw window
-        pygame.draw.rect(self.display, WHITE, BID_POSITION + BID_SIZE)
+        pygame.draw.rect(self.display, WHITE, WIN_POSITION + WIN_SIZE)
         # Draw contracts
         contracts_buttons = []
         for i in range(11):
@@ -71,18 +76,31 @@ class GUI:
             contracts_buttons.append(b)
         self.contract_clicked = False
         # Draw trumps
-        # Todo
+        trump_buttons = []
+        for i in range(6):
+            b = pygame.draw.rect(self.display, LIGHTGREY, TRUMP_POSITION[i] + TRUMP_SIZE[i])
+            self.set_trump_text(b, i)
+            trump_buttons.append(b)
+        self.trump_clicked = False
         # Draw Coinche, Validate, Pass
         # Todo
         # Render
         pygame.display.update()
-        return contracts_buttons
+        return contracts_buttons, trump_buttons
 
     def set_contract_text(self, button, idx):
         # Center the text in the button
         text = self.font.render(CONTRACTS[idx + 1], True, BLACK)
-        x_t, y_t = (button.width - text.get_width()) / 2, (button.height - text.get_height()) / 2
+        x_t, y_t = self.center_text(button, text)
         self.display.blit(text, (CONT_POSITION[idx][0] + x_t, CONT_POSITION[idx][1] + y_t))
+
+    def set_trump_text(self, button, idx):
+        text = self.font.render(TRUMPS[idx], True, BLACK)
+        x_t, y_t = self.center_text(button, text)
+        self.display.blit(text, (TRUMP_POSITION[idx][0] + x_t, TRUMP_POSITION[idx][1] + y_t))
+
+    def center_text(self, button, text):
+        return (button.width - text.get_width()) / 2, (button.height - text.get_height()) / 2
 
     def click_contract(self, pointer, buttons, prev_contract, contract):
         for i in range(len(buttons)):
@@ -99,3 +117,18 @@ class GUI:
                     pygame.display.update()
                 break
         return contract
+
+    def click_trump(self, pointer, buttons, trump):
+        for i in range(len(buttons)):
+            b = buttons[i]
+            if b.collidepoint(pointer):
+                pygame.draw.rect(self.display, GREY, TRUMP_POSITION[i] + TRUMP_SIZE[i])
+                self.set_trump_text(buttons[i], i)
+                if self.trump_clicked:
+                    pygame.draw.rect(self.display, LIGHTGREY, TRUMP_POSITION[trump] + TRUMP_SIZE[trump])
+                    self.set_trump_text(buttons[trump], trump)
+                self.trump_clicked = True
+                trump = i
+                pygame.display.update()
+                break
+        return trump
